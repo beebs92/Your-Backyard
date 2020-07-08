2822ec4f46f10ec0296407f72e5449788dd9b29b
Andrew Beban <apbeban@gmail.com>
2020-07-08 01:06:09 +0000

    add Mongodb Atlas

--- app.js
+++ app.js
@@ -16,7 +16,16 @@ var commentRoutes 		= require("./routes/comments"),
 	campgroundRoutes 	= require("./routes/campgrounds"),
 	indexRoutes 		= require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp", {useUnifiedTopology: true, useNewUrlParser: true,});
+// mongoose.connect("mongodb://localhost/yelp_camp", {
+// 	useUnifiedTopology: true, 
+// 	useNewUrlParser: true,
+// });
+mongoose.connect("mongodb+srv://beebs:niQvy49vS9qtkeeK@cluster0.xuyxg.mongodb.net/YelpCamp?retryWrites=true&w=majority", {
+	useUnifiedTopology: true, 
+	useNewUrlParser: true,
+	useCreateIndex: true,
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
