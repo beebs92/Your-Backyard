235b538223baf62e2c6e7eb834e8da82716cc3ec
Andrew Beban <apbeban@gmail.com>
2020-07-13 00:40:19 +0000

    add admin user access

--- middleware/index.js
+++ middleware/index.js
@@ -16,7 +16,7 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
                     return res.redirect("back");
                 }
                 //does user own campground
-                if (foundCampground.author.id.equals(req.user._id)) {
+                if (foundCampground.author.id.equals(req.user._id) || req.user.isAdmin) {
                     next();
                 } else {
                     req.flash("error", "You don't have permission to do that!");
@@ -41,7 +41,7 @@ middlewareObj.checkCommentOwnership = function checkCommentOwnership(req, res, n
                     return res.redirect("back");
                 }
                 //does user own comment
-                if (foundComment.author.id.equals(req.user._id)) {
+                if (foundComment.author.id.equals(req.user._id) || req.user.isAdmin) {
                     next();
                 } else {
                     req.flash("error", "You don't have permission to do that!");
