cf688d54f2382e3809c6e6178d6e732d9d1d3658
Andrew Beban <apbeban@gmail.com>
2020-07-16 02:45:16 +0000

    error handling bug fix

--- middleware/index.js
+++ middleware/index.js
@@ -9,18 +9,18 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
         Campground.findById(req.params.id, function(err, foundCampground) {
             if (err) {
                 req.flash("error", "Campground not found!");
-                res.redirect("back");
+                res.redirect("/campgrounds");
             } else {
                 if (!foundCampground) {
                     req.flash("error", "Item not found.");
-                    return res.redirect("back");
+                    return res.redirect("/campgrounds");
                 }
                 //does user own campground
                 if (foundCampground.author.id.equals(req.user._id) || req.user.isAdmin) {
                     next();
                 } else {
                     req.flash("error", "You don't have permission to do that!");
-                    res.redirect("back")
+                    res.redirect("/campgrounds")
                 }
             }
         });
@@ -34,11 +34,12 @@ middlewareObj.checkCommentOwnership = function checkCommentOwnership(req, res, n
     if (req.isAuthenticated()) {
         Comment.findById(req.params.comment_id, function(err, foundComment) {
             if (err) {
-                res.redirect("back");
+				req.flash("error", "Comment not found")
+                res.redirect("/campgrounds");
             } else {
                 if (!foundComment) {
                     req.flash("error", "Item not found.");
-                    return res.redirect("back");
+                    return res.redirect("/campgrounds");
                 }
                 //does user own comment
                 if (foundComment.author.id.equals(req.user._id) || req.user.isAdmin) {
